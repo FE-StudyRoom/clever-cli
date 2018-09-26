288f27be36ae2757bf49a0522e0b733deb33654a
sam.li(敏深) <liminshen15999@gmail.com>
2018-09-26 16:42:37 +0800

    create a function new...

---
+++ examples/repository/.cleverrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  projectName: 'test',//项目名称
+  dest: 'src',//目标文件夹
+  repository: {
+    protocal: 'github',//协议
+    branch: 'v1',//分支
+    origin: 'ant-design/ant-design-pro',//代码源
+    host: '',//
+  },
+  generateTpls: {    //配置工具模板  可以是文件夹/文件
+  }
+};
